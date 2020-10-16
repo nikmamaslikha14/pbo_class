8af550eb3b7026340350e1b6c0908724fffb9a78
nikmamaslikha14 <70304862+nikmamaslikha14@users.noreply.github.com>
2020-10-16 14:30:50 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ANI
+ */
+public class Main {
+    public static void main(String [] args) {
+        biodata_saya biodata = new biodata_saya();
+        biodata.namaLengkap();
+        biodata.kelas();
+        biodata.jurusan();
+        biodata.noAbsen();
+        biodata.tempatLahir();
+        biodata.tanggalLahir();
+        biodata.alamatLengkap();
+     }
+}
---
+++ biodata_saya.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ANI
+ */
+public class biodata_saya {
+     public void namaLengkap(){
+       System.out.println("Nama Lengkap : Nikmatul Maslikha");
+    }
+     
+    public void kelas(){
+       System.out.println(" Kelas : XI");
+    }
+    public void jurusan(){
+       System.out.println(" Jurusan : Rekayasa Perangkat Lunak");
+    }
+    public void noAbsen(){
+       System.out.println(" No.Absen : 25");
+    }
+    public void tempatLahir(){
+       System.out.println(" TempatLahir : Pasuruan");
+    }
+    public void tanggalLahir(){
+       System.out.println(" TanggalLahir : 14-09-2004 ");
+    }
+    public void alamatLengkap(){
+       System.out.println(" Alamat Lengkap: Dsn.Gunung Sari, Ds.Kertosari, Kec.Purwosari, Kab.Pasuruan, Provinsi.Jawa Timur, wilayah.Indonesia");
+    }
+  } 
